670ec0adf3ed45a2e2f614ea3d93754584282658
IsaacFigNewton <48895941+IsaacFigNewton@users.noreply.github.com>
2019-08-08 14:29:13 -0700

    Tried making a Winston function.

--- Winston.js
+++ Winston.js
@@ -1,5 +1,22 @@
 var c = document.getElementById("JSCanvas");
 var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-ctx.stroke();
+
+function circle(x, y, r) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI);
+	ctx.stroke();
+};
+
+function Winston(x, y, r) {
+	// face
+	circle(x, y, r);
+
+	// eyes
+	circle(x-43-(.25*r), y-59-(.25*r), 4*r/30);
+	circle(x+104+(.25*r), y-68-(.25*r), 4*r/30);
+
+	// mouth
+	circle(x+57+(.125*r), y+30+(.125*r), 128*r/300);
+};
+
+Winston(400, 400, 300);
